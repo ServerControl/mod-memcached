1fac15552b8ececa44b50c8a01a91ca16fc4d620
Jan Gehring <krimdomu@googlemail.com>
2011-06-30 14:45:54 +0200

    added configuration file support

--- lib/ServerControl/Module/Memcached.pm
+++ lib/ServerControl/Module/Memcached.pm
@@ -9,7 +9,7 @@ package ServerControl::Module::Memcached;
 use strict;
 use warnings;
 
-our $VERSION = '0.93';
+our $VERSION = '0.94';
 
 use ServerControl::Module;
 use ServerControl::Commons::Process;
@@ -29,15 +29,7 @@ sub help {
 
    printf "  %-30s%s\n", "--name=", "Instance Name";
    printf "  %-30s%s\n", "--path=", "The path where the instance should be created";
-   print "\n";
    printf "  %-30s%s\n", "--user=", "Memcache User";
-   printf "  %-30s%s\n", "--ip=", "Listen IP";
-   printf "  %-30s%s\n", "--port=", "Listen Port";
-   print "\n";
-   printf "  %-30s%s\n", "--try-large-memory", "Try to use large memory.";
-   printf "  %-30s%s\n", "--threads=", "Number of threads";
-   printf "  %-30s%s\n", "--backlog=", "Set backlog queue limit";
-   printf "  %-30s%s\n", "--max-memory-for-items=", "Max memory to use for items in megabytes.";
    print "\n";
    printf "  %-30s%s\n", "--create", "Create the instance";
    printf "  %-30s%s\n", "--start", "Start the instance";
@@ -51,19 +43,16 @@ sub start {
    my $pid_dir     = ServerControl::FsLayout->get_directory("Runtime", "pid");
 
    my ($name, $path)    = ($class->get_name, $class->get_path);
-   my $port             = ServerControl::Args->get->{'port'};
-   my $ip               = ServerControl::Args->get->{'ip'};
    my $user             = ServerControl::Args->get->{'user'};
-   my $max_mem_for_item = ServerControl::Args->get->{'max-memory-for-items'}?"-m " . ServerControl::Args->get->{'max-memory-for-items'} : "";
    my $pid_file         = "$path/$pid_dir/$name.pid";
-   my $large_memory     = ServerControl::Args->get->{'try-large-memory'}?"-L":"";
-   my $threads          = ServerControl::Args->get->{'threads'}?"-t " . ServerControl::Args->get->{'threads'} : "";
-   my $backlog          = ServerControl::Args->get->{'backlog'}?"-b " . ServerControl::Args->get->{'backlog'} : "";
    my $memcache_user    = ServerControl::Args->get->{'user'}?"-u " . ServerControl::Args->get->{'user'} : "";
 
    my $exec_file   = ServerControl::FsLayout->get_file("Exec", "memcached");
+   my $conf_file   = ServerControl::FsLayout->get_file("Configuration", "conf");
 
-   spawn("$path/$exec_file -d -p $port -l $ip $max_mem_for_item -P $pid_file $large_memory $threads -b $backlog $memcache_user");
+   my $params = eval { local(@ARGV, $/) = ($conf_file); <>; };
+
+   spawn("$path/$exec_file $memcache_user $params");
 }
 
 
